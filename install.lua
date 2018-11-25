f321b948342f500e3ec900097aee096a062cdd24
Erd <erd@conceptual.ca>
2018-11-25 13:27:08 -0500

    mkdir

--- install.lua
+++ install.lua
@@ -5,6 +5,10 @@ local installDir = "";
 if args[1] then
     installDir = args[1];
 end
+if installDir ~= "" then
+    local fs = require("filesystem");
+    fs.makeDirectory(installDir);
+end
 
 shell.execute('rm ' .. installDir .. '/boot/06_string_extentions.lua');
 
